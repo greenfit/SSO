ba3bdefb0a9697582d659ef4273a7e1be84f0ccc
liyu <liyu26@meituan.com>
2018-05-23 22:25:26 +0800

    新增Demo2,Demo3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SSO
 单点登录系统实例
+
+## Demo1
+验证Cookie不能跨域
+
+## Demo2
+多应用, UserID相同
+
+## Demo3
+SSO统一管理
---
+++ demo2/src/main/java/com/heleeos/sso/util/ResultBuilder.java
@@ -0,0 +1,18 @@
+package com.heleeos.sso.util;
+
+import com.heleeos.sso.bean.Result;
+
+/**
+ *
+ * Created by liyu on 2018/5/23.
+ */
+public class ResultBuilder {
+
+    public static <T> Result<T> build(int code, String message, T data) {
+        return new Result<>(code, message, data);
+    }
+
+    public static <T> Result<T> buildSuccess(T data) {
+        return build(200, "成功", data);
+    }
+}
